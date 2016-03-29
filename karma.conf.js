8a34efbd119af1676933107eff7aeb3e9ac40123
Luka Vidakovic <luka.vidakovic@gmail.com>
2016-03-30 00:44:16 +0200

    simple integration test for future redux releases

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
     ],
 
     preprocessors: {
-      './test/index.js': ['webpack'],
+      './test/**/*.js': ['webpack'],
     },
 
     webpack: require('./webpack.config'),
---
+++ test/redux-integration.js
@@ -0,0 +1,56 @@
+import 'phantomjs-polyfill'
+import { createStore, combineReducers } from 'redux'
+import { reducer, actions, selectors } from '../index'
+
+const store = createStore(
+  combineReducers({
+    _mount: reducer,
+  })
+)
+
+const mounted = selectors.mountState('_mount')
+const mountedProp = selectors.mountStateProp('_mount')
+
+describe('redux integration', () => {
+  it('should initialize with proper init values', () => {
+    // initial state
+    expect(store.getState()._mount).toEqual({
+      mountedOn: '',
+      routes: {},
+    })
+  })
+
+  it('should mount empty obj by default', () => {
+    // mount
+    store.dispatch(actions.mount('/first-mount'))
+    // read mounted values
+    expect(mounted(store.getState())).toEqual({})
+    expect(store.getState()._mount.mountedOn).toBe('/first-mount')
+  })
+
+  it('should set value to currently mounted obj', () => {
+    // set value
+    store.dispatch(actions.set('foo', 'bar'))
+    expect(mountedProp(store.getState())('foo')).toBe('bar')
+  })
+
+  it('should unmount and leave route values untouched', () => {
+    // unmount
+    store.dispatch(actions.unmount())
+    expect(store.getState()._mount.mountedOn).toEqual('')
+    // untouched values after unmount
+    expect(store.getState()._mount.routes['/first-mount'].foo).toBe('bar')
+  })
+
+  it('should mount with initial data if passed', () => {
+    // mount with initial data
+    store.dispatch(actions.mount('another/route', {test: 1, test2: 2}))
+    expect(mounted(store.getState())).toEqual({test: 1, test2: 2})
+  })
+
+  it('should clear mounted values correctly', () => {
+    // clear mounted values
+    store.dispatch(actions.clear())
+    expect(mounted(store.getState())).toEqual({})
+  })
+})
